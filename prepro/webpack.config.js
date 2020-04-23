a22d2f95455f8d144efa2a67d43dc97d691f15c2
Angel Camacho <adc.3191@gmail.com>
2020-04-23 00:51:36 -0400

    Se agregan los preprocesadores css

---
+++ prepro/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, "./src/js/home.jsx"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+  },
+  mode: "production",
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|woff|eot|ttf|svg|mp4|webm)$/i,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "assets/[name].[ext]",
+            limit: 100000,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Plugins",
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
