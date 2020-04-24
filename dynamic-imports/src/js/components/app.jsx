73d07ffc5915ccd67595fad12a28aaae27820e7a
Angel Camacho <adc.3191@gmail.com>
2020-04-23 22:19:44 -0400

    Dynamic library imports

---
+++ dynamic-imports/src/js/components/app.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import data from "./data.json";
+import Loader from "./loader";
+
+import logo from "../../images/platzi.png";
+import video from "../../video/que-es-core.mp4";
+
+import "../../sass/sass.scss";
+import "../../less/less.less";
+import "../../stylus/stylus.styl";
+
+function App() {
+  const [loaderList, setLoaderList] = useState([]);
+
+  async function handleClick() {
+    setLoaderList(data.loaders);
+    const { alerta } = await import("./alert.jsx");
+    alerta("Se ha cargado el módulo dinámicamente.");
+  }
+
+  return (
+    <div>
+      <div>
+        <img src={logo} width={100} />
+        <p className="sass">Esto es SASS</p>
+        <p className="less">Esto es LESS</p>
+        <p className="stylus">Esto es Stylus</p>
+        <p className="post-css">Esto es PostCSS</p>
+        <ul>
+          {loaderList.map((item) => (
+            <Loader {...item} key={item.id} />
+          ))}
+        </ul>
+        <button onClick={handleClick}>Mostrar Loaders</button>
+      </div>
+      <br></br>
+      <div>
+        <video src={video} width={360} controls poster={logo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ dynamic-imports/webpack.config.js
@@ -0,0 +1,119 @@
+const path = require("path");
+const webpack = require("webpack");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, "./src/js/home.jsx"),
+    contact: path.resolve(__dirname, "./src/js/contact.jsx"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+    chunkFilename: "js/[id]-[chunkhash].js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              url: false,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "stylus-loader",
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp4|webm)$/i,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "images/[name].[ext]",
+            limit: 0,
+          },
+        },
+      },
+      {
+        test: /\.(woff|eot|ttf|svg)$/i,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "fonts/[name].[ext]",
+            limit: 0,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "css/[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      filename: "index.html",
+      chunks: ["home"],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "contact.html"),
+      filename: "contact.html",
+      chunks: ["contact"],
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: require("./modules-manifest.json"),
+    }),
+  ],
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+};
